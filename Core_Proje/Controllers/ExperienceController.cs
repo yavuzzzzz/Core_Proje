46fa74d671e164856ca7d54502cfa80b3f43258d
yavuzzzzz <yvzylmz07@hotmail.com>
2024-09-20 01:24:44 +0300

    Experiences and Portfolio Side have done.

--- Core_Proje/Controllers/ExperienceController.cs
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -1,20 +1,55 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
-    public class ExperienceController : Controller
-    {
-        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
-        public IActionResult Index()
-        {
+	public class ExperienceController : Controller
+	{
+		ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+		public IActionResult Index()
+		{
 			ViewBag.v1 = "Deneyim Listesi";
 			ViewBag.v2 = "Deneyimler";
 			ViewBag.v3 = "Deneyim Listesi";
-            ViewBag.v4 = "Experience";
+			ViewBag.v4 = "Experience";
 			var values = experienceManager.TGetList();
-            return View(values);
-        }
-    }
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult AddExperience()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddExperience(Experience experience)
+		{
+			experienceManager.TAdd(experience);
+			return RedirectToAction("Index");
+
+		}
+		public IActionResult DeleteExperience(int id)
+		{
+			var value = experienceManager.TGetById(id);
+			experienceManager.TDelete(value);
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public IActionResult UpdateExperience(int id)
+		{
+			ViewBag.v1 = "Deneyim Listesi";
+			ViewBag.v2 = "Deneyimler";
+			ViewBag.v3 = "Deneyim Listesi";
+			var values = experienceManager.TGetById(id);
+			return View(values);
+		}
+		[HttpPost]
+		public IActionResult UpdateExperience(Experience experience)
+		{
+			experienceManager.TUpdate(experience);
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+	public class PortfolioController : Controller
+	{
+		PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+		public IActionResult Index()
+		{
+			ViewBag.v1 = "Proje Listesi";
+			ViewBag.v2 = "Projelerim";
+			ViewBag.v3 = "Proje Listesi";
+			var values = portfolioManager.TGetList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult AddPortfolio()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddPortfolio(Portfolio portfolio)
+		{
+			ViewBag.v1 = "Proje Listesi";
+			ViewBag.v2 = "Projelerim";
+			ViewBag.v3 = "Proje Ekleme";
+			portfolioManager.TAdd(portfolio);
+			return RedirectToAction("Index");
+		}
+	}
+}
