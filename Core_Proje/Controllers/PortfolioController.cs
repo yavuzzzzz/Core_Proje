8c35cc522938e882d62a562be83d4e92fa5310ba
yavuzzzzz <yvzylmz07@hotmail.com>
2024-09-21 02:53:12 +0300

    Fluent Validation has implemented to project

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Core_Proje.Controllers
 {
@@ -27,8 +31,61 @@ namespace Core_Proje.Controllers
 			ViewBag.v1 = "Proje Listesi";
 			ViewBag.v2 = "Projelerim";
 			ViewBag.v3 = "Proje Ekleme";
-			portfolioManager.TAdd(portfolio);
+
+			PortfolioValidator validations = new PortfolioValidator();
+			ValidationResult results = validations.Validate(portfolio);
+			if (results.IsValid)
+			{
+				portfolioManager.TAdd(portfolio);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+
+			}
+			return View();
+
+		}
+
+		public IActionResult DeletePortfolio(int id)
+		{
+			var values = portfolioManager.TGetById(id);
+			portfolioManager.TDelete(values);
 			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult UpdatePortfolio(int id)
+		{
+			ViewBag.v1 = "Proje Listesi";
+			ViewBag.v2 = "Projelerim";
+			ViewBag.v3 = "Proje Düzenleme";
+			var values = portfolioManager.TGetById(id);
+			return View(values);
+		}
+
+		[HttpPost]
+		public IActionResult UpdatePortfolio(Portfolio portfolio)
+		{
+			PortfolioValidator validations = new PortfolioValidator();
+			ValidationResult results = validations.Validate(portfolio);
+			if (results.IsValid)
+			{
+				portfolioManager.TUpdate(portfolio);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
 		}
 	}
 }
